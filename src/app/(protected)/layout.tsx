5be9e01b7df19f0df15148b6cab4c46238848fd8
syaeful16 <syaefulloharnas16@gmail.com>
2025-10-30 17:11:42 +0700

    Refactor ProtectedLayout component to simplify structure and remove unnecessary flex properties

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -2,9 +2,9 @@ import Navbar from "@/components/Navbar"
 
 export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="min-h-screen flex flex-col">
+        <div className="min-h-screen">
             <Navbar />
-            <main className="flex-1 p-6">{children}</main>
+            <main>{children}</main>
         </div>
     )
 }
